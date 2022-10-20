36b6b96a84e036979e86de320fcefefdf4eea4b1
Muhammad Enamul Haque <muhammad.enamul.haque79@gmail.com>
2022-10-21 02:20:55 +0600

    signinwithgoogle

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
+import app from './firebase.init';
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup
+} from 'firebase/auth';
+import { useState } from 'react';
 
+const auth = getAuth(app);
 function App() {
+  const[user,setUser]=useState({});
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    // console.log('working');
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        setUser(user);
+      }).catch((error) => {
+        console.error(error);
+      })
+  }
+
   return (
     <div className="App">
-      <h2>Yes, I can do!!</h2>
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      <h2>Name:{user.displayName}</h2>
+      <img style={{borderRadius:'100%'}} src={user.photoURL} alt="" />
+      <h3>{user.email}</h3>
     </div>
   );
 }
--- src/__Steps.js
+++ src/__Steps.js
@@ -3,5 +3,13 @@
 2.Register app;
 3.npm install firebase(yarn add firebase);
 4.Initialized firebase into firebase.init.js;
-5.
+5.export default app;
+6.import app from firebase.init.js;
+7.const auth=getAuth(app) into app.js;
+8.import {getAuth} from 'firebase/auth';
+9.Authentication(turn on desire provider);
+10.Create GoogleAuthProvider and also import {GoogleAuthProvider} from 'firebase/auth';
+11.signInWithPopup and also import {signInWithPopup} from "firebase/auth"; into handleGoogleSignIn;
+12.Connect handleGoogleSignIn with onClick;
+
  */
